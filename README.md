d36e870c3e48dd7de085cd0dc16b45d15d184105
Dmitrii Sosedov <dsosedov@pivotal.io>
2020-02-10 11:34:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Overview
+
+An ASP.NET Framework 4.7.2 MVC application that uses RabbitMQ
---
+++ src/Web/Controllers/ProbeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class ProbeController : Controller
+    {
+        const string QueueName = @"rabbitmq-mvc-dotnetframework-probe";
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            try
+            {
+                ViewData["Status"] = "Connection established successfully.";
+            }
+            catch (Exception ex)
+            {
+                ViewData["Status"] = ex.Message;
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Send()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Send(Models.Message message)
+        {
+            try
+            {
+                ViewData["Status"] = "Message sent successfully.";
+            }
+            catch (Exception ex)
+            {
+                ViewData["Status"] = ex.Message;
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Receive()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Receive(Models.Message message)
+        {
+            try
+            {
+                string msgBody;
+
+                msgBody = "foo";
+
+                ViewData["Status"] = msgBody;
+            }
+            catch (Exception ex)
+            {
+                ViewData["Status"] = ex.Message;
+            }
+
+            return View();
+        }
+    }
+}
